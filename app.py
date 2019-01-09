301dee4aa55b4ee331620dd8be069e27250aa8d2
spnarkdnark <samuelfudala@gmail.com>
2019-01-08 20:18:25 -0800

    first commit

---
+++ app.py
@@ -0,0 +1,62 @@
+import os
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "bookdatabase.db"))
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+
+db = SQLAlchemy(app)
+
+
+class Book(db.Model):
+    title = db.Column(db.String(80), unique=True, nullable=False,primary_key=True)
+
+    def __repr__(self):
+        return "<Title: {}>".format(self.title)
+
+
+@app.route("/", methods = ["GET","POST"])
+def welcome():
+
+    if request.form:
+        try:
+            book = Book(title=request.form.get("title"))
+            db.session.add(book)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            print('Failed to add book!')
+            print(e)
+
+    books = Book.query.all()
+    return render_template('welcome.html',books = books)
+
+
+@app.route("/update", methods=["POST"])
+def update():
+    try:
+        newtitle = request.form.get("newtitle")
+        oldtitle = request.form.get("oldtitle")
+        book = Book.query.filter_by(title=oldtitle).first()
+        book.title = newtitle
+        db.session.commit()
+    except Exception as e:
+        print('Failed to update book')
+        print(e)
+    return redirect('/')
+
+@app.route("/delete",methods=["POST"])
+def delete():
+    title = request.form.get("title")
+    book = Book.query.filter_by(title=title).first()
+    db.session.delete(book)
+    db.session.commit()
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
